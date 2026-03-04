2a4b5828584052cc1a3ebf5d8ff2ff1d80ded200
safin <mahfuzsafin@gmail.com>
2026-03-04 21:34:07 +0600

    js syntax and comment

---
+++ 03-js-syntax-and-comment/apps.js
@@ -0,0 +1,14 @@
+// every progrtaming language a kisu rules thake code lekhar jonno agula ke bole syntax, jodi rules break hoy tahole program syntax error dey
+
+let firstName = 'Mahfuj';
+// ekhane jmn first a deoya ase variable ta ki rokon hobe, tarpore deoya ase variable er name and tarpor value and last a deoya ase close, eta akta javascript er syntax.
+
+// (firstname):snake case. (FirstName): pascal case. (firstName): camel case, standerd.
+
+console.log(firstName);
+
+// jokhon value variable operator niye kono kisu lekha hoy and setar output a kono akta value pai sei value k bola hoy expression
+
+// this is single line comment,
+
+/* this is pultiple line comment */
