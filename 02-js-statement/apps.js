82d8f59207d75ed295bf5ce14edbb012a12eefa6
safin <mahfuzsafin@gmail.com>
2026-03-04 19:35:41 +0600

    js statement

---
+++ 02-js-statement/apps.js
@@ -0,0 +1,16 @@
+console.log('hello');
+
+// amra jokhon kono instruction kono program k dei tokhon take statement bole, duita statement alada korte hole (;) semiclone use korte hoy
+document.getElementById('title').innerHTML = 'Hello world';
+
+
+let sum = 150 + 40
+console.log(sum)
+
+
+// statement dui line ao lekha jay
+if (sum > 100) {
+  console.log('expensive');
+} else {
+  console.log('cheep');
+}
